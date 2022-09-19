1e053beec032b95a1181a61a48319f10915e15aa
potter1230 <potter12301@gmail.com>
2022-09-19 18:14:16 +0900

    프로젝트 생성

---
+++ src/components/Fruit.vue
@@ -0,0 +1,20 @@
+<template>
+    <li>{{ name }}?!</li>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            default: ""
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss"> // scoped: 유효범위, 이 속성 추가 하면 style이 여기만 적용됨
+    h1 {
+        color: red;
+    }
+</style>
